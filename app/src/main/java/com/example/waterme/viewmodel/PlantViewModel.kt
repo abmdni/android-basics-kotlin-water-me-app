b4fdc0fce34e15c0e062a906766583bf9b4445fd
abmdni <abdessamad.yadini@gmail.com>
2022-06-27 03:54:35 +0100

    schedule notifications using workManager

--- app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
+++ app/src/main/java/com/example/waterme/viewmodel/PlantViewModel.kt
@@ -18,11 +18,12 @@ package com.example.waterme.viewmodel
 import android.app.Application
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.work.WorkManager
+import androidx.work.*
 import com.example.waterme.data.DataSource
+import com.example.waterme.worker.WaterReminderWorker
 import java.util.concurrent.TimeUnit
 
-class PlantViewModel(application: Application): ViewModel() {
+class PlantViewModel(private val application: Application): ViewModel() {
 
     val plants = DataSource.plants
 
@@ -32,11 +33,19 @@ class PlantViewModel(application: Application): ViewModel() {
         plantName: String
     ) {
         // TODO: create a Data instance with the plantName passed to it
+        val data = Data.Builder().putString(WaterReminderWorker.nameKey, plantName).build()
 
         // TODO: Generate a OneTimeWorkRequest with the passed in duration, time unit, and data
         //  instance
+        val req = OneTimeWorkRequestBuilder<WaterReminderWorker>()
+            .setInputData(data)
+            .setInitialDelay(duration, unit)
+            .build()
 
         // TODO: Enqueue the request as a unique work request
+        WorkManager
+            .getInstance(application)
+            .enqueueUniqueWork(plantName, ExistingWorkPolicy.REPLACE, req)
     }
 }
 
